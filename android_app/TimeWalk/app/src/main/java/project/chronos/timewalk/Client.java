0431d9d638af08e4d33f36228d3bc9cea5030607
Kiannoc <kian.noctor@gmail.com>
2017-09-06 15:54:32 +1000

    added image names and gps

--- android_app/TimeWalk/app/src/main/java/project/chronos/timewalk/Client.java
+++ android_app/TimeWalk/app/src/main/java/project/chronos/timewalk/Client.java
@@ -140,13 +140,109 @@ public class Client {
         }
     }
 
-    Data<ArrayList<String>> landmarks()
+    public Data<ArrayList<String>> landmarks()
     {
         try {
             Landmarks land = new Landmarks();
             return land.execute("Brisbane").get();
         } catch (Exception e) {
             Log.e(CLIENT_LOG, "landmarks interrupted: " + e.getMessage());
+        }
+        return null;
+    }
+
+    //  ** IMAGE NAMES **
+
+    private class ImageNames extends AsyncTask<String, Void, Data<ArrayList<String>>> {
+
+        @Override
+        protected Data<ArrayList<String>> doInBackground(String... strings) {
+            Data<ArrayList<String>> data = new Data<>();
+
+            _output.println(to_string(Request.LIST_IMAGES) + " " +
+                    strings[0] + " " + strings[1] + " " + strings[2] + ";");
+
+            try {
+                Transfer code = to_code(_input.readLine());
+                data.code = code;
+
+                if (code != Transfer.TEXT) {
+                    data.error_msg = _input.readLine();
+                    data.failed = true;
+                } else {
+                    data.result = new ArrayList<>();
+                    for (String image_name : _input.readLine().split(SEPERATOR))
+                        data.result.add(image_name);
+                }
+
+            } catch (Exception e) {
+                Log.e(CLIENT_LOG, "Images Failed: " + e.getLocalizedMessage());
+                data.result = null;
+                data.failed = true;
+                data.error_msg = e.getMessage();
+                data.code = Transfer.EXCEPTION_FAIL;
+            }
+
+            return data;
+        }
+    }
+
+    public Data<ArrayList<String>> imageNames(String landmark) {
+        try {
+            ImageNames images = new ImageNames();
+            return images.execute("Brisbane", landmark).get();
+        } catch (Exception e) {
+            Log.e(CLIENT_LOG, "Image Names Interrupted: " + e.getMessage());
+        }
+        return null;
+    }
+
+    //  ** GPS **
+
+    public static class GPSCoord {
+        public double longitude;
+        public double latitude;
+    }
+
+    private class GPS extends AsyncTask<String, Void, Data<GPSCoord>> {
+
+        @Override
+        protected Data<GPSCoord> doInBackground(String... strings) {
+            Data<GPSCoord> data = new Data<>();
+
+            try {
+
+                Transfer code = to_code(_input.readLine());
+                data.code = code;
+
+                if (code != Transfer.TEXT) {
+                    data.failed = true;
+                    data.error_msg = _input.readLine();
+                } else {
+                    String[] gps_str_arr = _input.readLine().split(" ");
+                    data.result = new GPSCoord();
+                    data.result.longitude = Double.parseDouble(gps_str_arr[0]);
+                    data.result.latitude = Double.parseDouble(gps_str_arr[0]);
+                }
+
+            } catch (Exception e) {
+                Log.e(CLIENT_LOG, "GPS Failed: " + e.getMessage());
+                data.result = null;
+                data.failed = true;
+                data.error_msg = e.getMessage();
+                data.code = Transfer.EXCEPTION_FAIL;
+            }
+
+            return data;
+        }
+    }
+
+    public Data<GPSCoord> gpsCoord(String landmark) {
+        try {
+            GPS gps = new GPS();
+            return gps.execute("Brisbane", landmark).get();
+        } catch (Exception e) {
+            Log.e(CLIENT_LOG, "GPS Coord Interrupted: " + e.getMessage());
         }
         return null;
     }
